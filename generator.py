49b75e785e0f84cddef671ca56a29b369b93875c
MaxLZE <maxlewze@gmail.com>
2026-03-04 21:04:33 +0800

    changed it to stack

--- generator.py
+++ generator.py
@@ -1,31 +1,39 @@
 from maze import Maze
 from cell import Cell
 import random
+
 class Generator:
-    def __init__(self,maze):
+    def __init__(self, maze):
         self.maze = maze
-    def backtrack(self,cell):
+
+    def backtrack(self, cell):
+        stack = []
+        stack.append(cell)
         cell.visited = True
-        neighbours = self.maze.get_neighbours(cell)
-        unvisited = []
-        for neighbour in neighbours: 
-            if neighbour.visited == False:
-                unvisited.append(neighbour)
-        if len(unvisited) == 0:
-            return
-        next_cell = random.choice(unvisited)
-        if next_cell.row > cell.row :
-            cell.bottom = False
-            next_cell.top= False
-        elif next_cell.row < cell.row : 
-            cell.top = False
-            next_cell.bottom = False
-        elif next_cell.column > cell.column:
-            cell.right = False
-            next_cell.left = False
-        elif next_cell.column < cell.column:
-            cell.left = False
-            next_cell.right = False
-        self.backtrack(next_cell)
+        while len(stack) > 0:
+            current = stack.pop()
+            neighbours = self.maze.get_neighbours(current)
+            unvisited = []
+            for neighbour in neighbours:
+                if neighbour.visited == False:
+                    unvisited.append(neighbour)
+            if len(unvisited) == 0:
+                continue
+            next_cell = random.choice(unvisited)
+            if next_cell.row > current.row:
+                current.bottom = False
+                next_cell.top = False
+            elif next_cell.row < current.row:
+                current.top = False
+                next_cell.bottom = False
+            elif next_cell.column > current.column:
+                current.right = False
+                next_cell.left = False
+            elif next_cell.column < current.column:
+                current.left = False
+                next_cell.right = False
+            stack.append(current)
+            next_cell.visited = True
+            stack.append(next_cell)
 
         
