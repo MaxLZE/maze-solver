136886ddcfc429b08cdbd7089f252e93a6772793
MaxLZE <maxlewze@gmail.com>
2026-03-04 18:15:26 +0800

    Added getneighbours method

--- maze.py
+++ maze.py
@@ -4,4 +4,13 @@ class Maze:
         self.rows = rows
         self.columns = columns
         self.grid = [[Cell(r,c) for c in range(self.columns)] for r in range(self.rows)]
-        
+    def get_neighbours(self,cell):
+        validneighbours = []
+        r = cell.row
+        c = cell.column
+        neighbours = (r+1,c) , (r-1,c), (r,c-1), (r,c+1)
+        for nr,nc in neighbours:
+            if 0 <= nr < self.rows and 0 <= nc < self.columns:
+                validneighbours.append(self.grid[nr][nc])
+        return validneighbours
+                    
